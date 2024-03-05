38990af14c105c356083359b6420bf53b0561e59
Aswin Krishna <aswinkrishna005@gmail.com>
2024-03-05 11:28:51 +0530

    login commit

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,14 +1,22 @@
 var db=require('../config/connection')
-module.exports={
-addProduct:(product,callback)=>{
-    console.log(product)
-    db.get().collection('product').insertOne(product)
+var collection=require('../config/collections')
 
-    .then((data)=>{
-        console.log(data)
-        callback(data)
+module.exports={
+
+    addProduct:(product,callback)=>{
+        console.log(product)
+        db.get().collection('product').insertOne(product)
+        .then((data)=>{
+        
+        callback(data.insertedId);
     })
 
-}
+},
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-      name:"SAMSUNG Galaxy S24 Ultra 5G",
-      category:"Mobile",
-      description:"(Titanium Gray, 256 GB)  (12 GB RAM)",
-      image:"https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/f/n/u/-original-imagx9egm9mgmvab.jpeg?q=90&crop=false"
-    },
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('user/view-products',{products})
 
-    {
-      name:"Apple iPhone 15 Pro",
-      category:"Mobile",
-      description:" (Blue Titanium, 128 GB)",
-      image:"https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1694674264/Croma%20Assets/Communication/Mobiles/Images/300785_0_ikcks6.png"
-    },
+  })
+});
 
-    {
-      name:"OnePlus 11 5G",
-      category:"Mobile",
-      description:"(Eternal Green, 256 GB)  (16 GB RAM)",
-      image:"https://oasis.opstatics.com/content/dam/oasis/page/2023/na/oneplus-11/specs/green-img.png"
-    },
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+});
 
-    {
-      name:"Oppo F5",
-      category:"Mobile",
-      description:" (Black, 64 GB)  (6 GB RAM) ",
-      image:"https://www.refurbkart.com/cdn/shop/files/Oppo_F5_Youth_grande.jpg?v=1683876177"
-    }
-  ]
-
-  res.render('index', { products, admin:false});
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
 });
 
 module.exports = router;
